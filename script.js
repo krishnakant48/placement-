c4a29116e2d0e06478f78e9bcabd8ee19b7d60b9
krishnakant48 <rabhi4127@gmail.com>
2021-09-24 09:57:32 +0530

    static page

---
+++ script.js
@@ -0,0 +1,145 @@
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
+//image gallery
+const myslide = document.querySelectorAll(".myslide"),
+  dot = document.querySelectorAll(".dot");
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+  counter += 1;
+  slidefun(counter);
+}
+function plusSlides(n) {
+  counter += n;
+  slidefun(counter);
+  resetTimer();
+}
+function currentSlide(n) {
+  counter = n;
+  slidefun(counter);
+  resetTimer();
+}
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+  let i;
+  for (i = 0; i < myslide.length; i++) {
+    myslide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  if (n > myslide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = myslide.length;
+  }
+  myslide[counter - 1].style.display = "block";
+  dot[counter - 1].className += " active";
+}
+
+//whatsapp
+
+$("#myDiv").floatingWhatsApp({
+  phone: "5491133359850",
+  popupMessage: "Chat with us and get a quotation as soon as possible.",
+  message: "I'd like to know about the training activities in detail.",
+  showPopup: true,
+  showOnIE: false,
+  headerTitle: "Welcome!",
+
+  position: "right",
+  // buttonImage: '{% static "<img src=img/whatsapp.svg />" %}',
+});
+
+document.querySelector("#btn").addEventListener("click", function () {
+  alert("Thanks For Contacting🙂");
+});
